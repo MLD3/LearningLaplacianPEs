18ed5a8ca404f49ad19ff1ddcd3624eee0743a54
michaelbryantito <mbito@umich.edu>
2025-03-11 14:33:34 -0400

    Update search.sh

--- search.sh
+++ search.sh
@@ -8,9 +8,9 @@ declare -a datasets=("Cora")
 for data in "${datasets[@]}"
 do
 
-    # python3 main_search_benchmarks.py --lrs_b .01 .001 --lrs_m 1 5 --h_dims 128  --pos_dims 64 128 --num_layers 2 --dropouts 0 .2 --Ks 64 128 --l12s .01 .001 .0001 0 --model_name MLP_LLPE --pos_method llpe --data_name "$data"
-    # python3 main_search_benchmarks.py --lrs_b .01 .001 --lrs_m 1 5 --h_dims 128  --pos_dims 64 128 --num_layers 2 --dropouts 0 .2 --Ks 64 128 --l12s .01 .001 .0001 0 --model_name SAGE_LLPE --pos_method llpe --data_name "$data"
-    # python3 main_search_benchmarks.py --lrs_b .01 .001 --lrs_m 1 5 --h_dims 128  --pos_dims 64 128 --num_layers 2 --dropouts 0 --lns 0 .00001 --Ks 64 128 --l12s .01 .001 .0001 0 --model_name GT_LLPE --pos_method llpe --data_name "$data"
+    # python3 main_search.py --lrs_b .01 .001 --lrs_m 1 5 --h_dims 128  --pos_dims 64 128 --num_layers 2 --dropouts 0 .2 --Ks 64 128 --l12s .01 .001 .0001 0 --model_name MLP_LLPE --pos_method llpe --data_name "$data"
+    # python3 main_search.py --lrs_b .01 .001 --lrs_m 1 5 --h_dims 128  --pos_dims 64 128 --num_layers 2 --dropouts 0 .2 --Ks 64 128 --l12s .01 .001 .0001 0 --model_name SAGE_LLPE --pos_method llpe --data_name "$data"
+    # python3 main_search.py --lrs_b .01 .001 --lrs_m 1 5 --h_dims 128  --pos_dims 64 128 --num_layers 2 --dropouts 0 --lns 0 .00001 --Ks 64 128 --l12s .01 .001 .0001 0 --model_name GT_LLPE --pos_method llpe --data_name "$data"
     
 done
 
